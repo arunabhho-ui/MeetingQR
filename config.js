6cb84dc5c44283e262f5df447500a898823f03bc
arunabhho-ui <arunabhho@gmail.com>
2026-02-16 16:14:14 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+
+  event: {
+    name: "Meeting 1",
+    date: "2026-02-15",
+    startTime: "10:00",
+    durationMinutes: 60
+  },
+
+  location: {
+    latitude: 12.9716,
+    longitude: 77.5946,
+    radius: 100
+  },
+
+  mailerScriptURL: "https://script.google.com/macros/s/AKfycbxQpIN6wwHJ2x8I4-G2YEdqlBIw5pvegTotKeA-UQRwoZ66z0yFT56lg6qj_Ha36KVktw/exec"
+
+};
