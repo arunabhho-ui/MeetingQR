13e41ab72c37c5c91d673da8aeae0a6b83e93d12
arunabhho-ui <arunabhho@gmail.com>
2026-02-16 15:14:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
 
   event: {
-    name: "Meeting 1",
-    date: "2026-02-15",
-    startTime: "10:00",
-    durationMinutes: 60
+    name: "",
+    date: "",
+    startTime: "",
+    durationMinutes: 
   },
 
   location: {
-    latitude: 12.9716,
-    longitude: 77.5946,
-    radius: 100
+    latitude: ,
+    longitude: ,
+    radius: 
   },
 
-  googleScriptURL: "https://script.google.com/macros/s/AKfycbygVzLES1366NrA4UQZ08s6tRoWGL_q-pwbAnHSXqax2MC9xzti7b8oBF81lQybZitM/exec"
+  googleScriptURL: "https://script.google.com/macros/s/AKfycbxQpIN6wwHJ2x8I4-G2YEdqlBIw5pvegTotKeA-UQRwoZ66z0yFT56lg6qj_Ha36KVktw/exec"
 
 };
